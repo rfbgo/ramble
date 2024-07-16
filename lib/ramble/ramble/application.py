b1263c28c9449b98419992d70e68a117ea62b227
David Pomeroy <dpomeroy@google.com>
2024-07-16 22:42:44 +0000

    fixup! Add FOM types

--- lib/ramble/ramble/application.py
+++ lib/ramble/ramble/application.py
@@ -53,7 +53,7 @@ from ramble.util.shell_utils import source_str
 from ramble.workspace import namespace
 
 from ramble.language.application_language import ApplicationMeta
-from ramble.language.shared_language import SharedMeta, register_builtin, register_phase
+from ramble.language.shared_language import SharedMeta, FomType, register_builtin, register_phase
 from ramble.error import RambleError
 from ramble.util.output_capture import output_mapper
 
@@ -1997,12 +1997,10 @@ class ApplicationBase(metaclass=ApplicationMeta):
 
             mod_vars = mod.modded_variables(self)
 
-            print(f'mod_figures_of_merit = {mod.figures_of_merit}')
-
             for fom, fom_def in mod.figures_of_merit.items():
                 fom_definitions[fom] = {"origin": f"{mod}", "origin_type": "modifier"}
                 for attr in fom_def.keys():
-                    if isinstance(fom_def[attr], list):
+                    if isinstance(fom_def[attr], (list, FomType)):
                         fom_definitions[fom][attr] = fom_def[attr].copy()
                     else:
                         fom_definitions[fom][attr] = self.expander.expand_var(
@@ -2010,8 +2008,6 @@ class ApplicationBase(metaclass=ApplicationMeta):
                         )
 
         for fom, conf in fom_definitions.items():
-            #print(f'fom = {fom}')
-            #print(f'conf = {conf}')
             log_path = self.expander.expand_var(conf["log_file"])
 
             if log_path not in files:
